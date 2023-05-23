c0fab4f416b2d5efe6e65f1c3391859d1f9f5a16
masahiro96848 <feetwcpic9436@gmail.com>
2023-05-23 22:05:23 +0900

    EsLintとPrettierを導入。実際に動くか検証。またimportのbaseUrlを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
+import '@/globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
