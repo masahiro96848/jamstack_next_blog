9cf1eacf3c90f6c40356d43c7ecc20706ee5842f
masahiro96848 <feetwcpic9436@gmail.com>
2023-05-26 00:05:19 +0900

    sass sass-loaderをインストール。cssModulesを使用。postcss.config.jsのtailwindを削除

---
+++ app/components/layouts/Header.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from '@/styles/modules/header.module.scss'
+
+const Header = () => {
+  return (
+    <div className="">
+      <div className="">
+        <div className={styles.main}>
+          <p>テスト</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Header from '@/components/layouts/Header'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Header />
+    </div>
+  )
 }
