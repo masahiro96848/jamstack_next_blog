82b16dd28855815351302aeb50c13b77ff694c85
masahiro96848 <feetwcpic9436@gmail.com>
2023-05-26 23:46:50 +0900

    ヘッダーのタイトルやリンクを追加。レスポンシブも対応中

--- app/components/layouts/Header.tsx
+++ app/components/layouts/Header.tsx
@@ -4,10 +4,25 @@ import styles from '@/styles/modules/header.module.scss'
 
 const Header = () => {
   return (
-    <div className="">
-      <div className="">
+    <div className={styles.container}>
+      <div className={styles.top}>
         <div className={styles.main}>
-          <p>テスト</p>
+          <Link href="/">
+            <div className={styles.title}>
+              <h1>ZEROne</h1>
+            </div>
+          </Link>
+          <div className={styles.link}>
+            <Link href="">
+              <h2>トップ</h2>
+            </Link>
+            <Link href="">
+              <h2>このブログについて</h2>
+            </Link>
+            <Link href="">
+              <h2>プロフィール</h2>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
