b60e57c860bc0f370284f5d76c58390b5ef4c66b
masahiro96848 <feetwcpic9436@gmail.com>
2023-06-27 14:40:49 +0900

    profileApiを作成

---
+++ app/apis/ProfileApi.ts
@@ -0,0 +1,35 @@
+/**
+ * ProfileApi.ts
+ * プロフィールAPI
+ * @package apis
+ */
+/* config */
+import globalAxios from '@/config/globalAxios'
+
+import { initProfileState } from '@/constants/initState'
+/* types */
+import { ProfileType } from '@/types/Profile'
+
+/**
+ * constant
+ */
+const BASE_URL = `${
+  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/'
+}/profile/`
+
+/**
+ * プロフィール詳細取得
+ * @returns {Promise<ProfileType>}
+ */
+export const getProfileApi = async (): Promise<ProfileType> => {
+  let profile: ProfileType = initProfileState
+  try {
+    const res = await globalAxios.get(BASE_URL + 'user')
+    if (res?.data) {
+      profile = res.data
+    }
+  } catch (error) {
+    throw new Error(`API ERROR: getProfileApi`)
+  }
+  return profile
+}
