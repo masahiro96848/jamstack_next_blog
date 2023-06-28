189fc25a58aed1309b796a004cc6e6e1f8dc93d8
masahiro96848 <feetwcpic9436@gmail.com>
2023-06-28 14:58:57 +0900

    categoryのAPIを作成。一覧と詳細のAPIをそれぞれ作成

---
+++ app/types/Category.ts
@@ -0,0 +1,16 @@
+/**
+ * カテゴリーのinterface
+ * @package types
+ */
+
+/**
+ * CategoryType
+ */
+export interface CategoryType {
+  id: string
+  name: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+}
