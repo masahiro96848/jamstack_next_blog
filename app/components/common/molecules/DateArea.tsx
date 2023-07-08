9aff8f1f4b0e649619bca221264aa607cd14bd96
masahiro96848 <feetwcpic9436@gmail.com>
2023-07-08 22:35:05 +0900

    dateAreaに更新日の日付を追加

--- app/components/common/molecules/DateArea.tsx
+++ app/components/common/molecules/DateArea.tsx
@@ -5,6 +5,8 @@
 
 import React from 'react'
 import { AiOutlineClockCircle } from 'react-icons/ai'
+import { BsArrowClockwise } from 'react-icons/bs'
+
 /* logic */
 import { changeYearMonthDateLogic } from '@/logic/DateLogic'
 /* styles */
@@ -22,10 +24,14 @@ export const DateArea: React.FC<Props> = (props: Props) => {
   const { date, size } = props
   return (
     <div className={styles.container}>
-      <div className={styles.icon}>
+      <div className={styles.createdIcon}>
         <AiOutlineClockCircle size={size} />
       </div>
       <p className={styles.date}>{changeYearMonthDateLogic(date)}</p>
+      <div className={styles.updatedIcon}>
+        <BsArrowClockwise size={size} />
+      </div>
+      <p className={styles.date}>2022-04-22</p>
     </div>
   )
 }
