1c6709ef4b483562778593e731ad7fa72c7b6e0f
masahiro96848 <feetwcpic9436@gmail.com>
2023-07-14 21:34:15 +0900

    プロフィール、プライバシポリシーにmeta tagを追加

---
+++ app/policy/layout.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from 'next'
+import '@/styles/globals.scss'
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `免責事項・プライバシーポリシー | ZEROne Blog`,
+    description: 'ZEROneは学んだこと、役に立つ知識を発信していくブログです',
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,10 +1,13 @@
 import { Metadata } from 'next'
 import '@/styles/globals.scss'
 
-export const metadata: Metadata = {
-  title: 'トップページ',
-  description: 'ZEROneは学んだこと、役に立つ知識を発信していくブログです',
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `プロフィール | ZEROne Blog`,
+    description: 'ZEROneは学んだこと、役に立つ知識を発信していくブログです',
+  }
 }
+
 export default function RootLayout({
   children,
 }: {
