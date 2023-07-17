404196e1c1007a85ef939437706bf0618afd5d85
masahiro96848 <feetwcpic9436@gmail.com>
2023-07-17 09:36:43 +0900

    gtagを作成

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import { GoogleAnalytics } from './components/common/atoms/GoogleAnalytics'
+
+const Head = () => {
+  return (
+    <>
+      <GoogleAnalytics />
+    </>
+  )
+}
+
+export default Head
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { Metadata } from 'next'
+import Head from './head'
 import '@/styles/globals.scss'
 
 export const metadata: Metadata = {
   title: 'トップページ',
   description: 'ZEROneは学んだこと、役に立つ知識を発信していくブログです',
 }
+
 export default function RootLayout({
   children,
 }: {
@@ -12,6 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <Head />
       <body>{children}</body>
     </html>
   )
